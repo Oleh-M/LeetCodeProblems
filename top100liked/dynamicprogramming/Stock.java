ed68f69e4a92462403358ac9ba03a5079e00b004
Oleh Mykhash <Myhash.Oleg@gmail.com>
2021-08-28 16:42:46 +0300

    reorganizing structure add stock task

---
+++ top100liked/dynamicprogramming/Stock.java
@@ -0,0 +1,27 @@
+package buysellstocks;
+
+public class Solution {
+    public static void main(String[] args) {
+//        System.out.println(maxProfit(new int[] {7,1,5,3,6,4}));
+//        System.out.println(maxProfit(new int[] {7,6,4,3,1}));
+        System.out.println(maxProfit(new int[] {8,5,2,4,54,1,3,12,32,4,5,6,7,7,43,4,2}));
+    }
+
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
+
+        while (right < prices.length) {
+            if(prices[right] > prices[left]) {
+                maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+            } else left = right;
+
+            right++;
+        }
+
+        return maxProfit;
+    }
+}

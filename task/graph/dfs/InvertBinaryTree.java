5c54b81a4187bab994645cb15693ba8d66a5f0d7
Oleh Mykhash <Myhash.Oleg@gmail.com>
2021-11-26 18:55:33 +0200

    Add last easy dfs tasks

---
+++ task/graph/dfs/InvertBinaryTree.java
@@ -0,0 +1,54 @@
+package graph.dfs;
+
+import graph.TreeNode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class InvertBinaryTree {
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    public static TreeNode invertTreeIterative(TreeNode root) {
+        if(root == null) return null;
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.push(root);
+
+        TreeNode node;
+        while(!queue.isEmpty()) {
+            node = queue.poll();
+            if(node.left != null) {
+                queue.add(node.left);
+            }
+            if(node.right != null) {
+                queue.add(node.right);
+            }
+
+            var temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        var tree = new TreeNode(4,
+                new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+                new TreeNode(7, new TreeNode(6), new TreeNode(9)));
+
+//        var inverted = invertTree(tree);
+        var invertedIter = invertTreeIterative(tree);
+        System.out.println();
+    }
+}
--- task/graph/dfs/SubtreeOfAnotherTree.java
+++ task/graph/dfs/SubtreeOfAnotherTree.java
@@ -4,17 +4,18 @@ import graph.TreeNode;
 
 public class SubtreeOfAnotherTree {
     public static boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(subRoot == null) return true;
-        if(root == null) return false;
+        if (subRoot == null) return true;
+        if (root == null) return false;
         if (verifySubTree(root, subRoot)) return true;
 
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        return isSubtree(root.left, subRoot) ||
+                isSubtree(root.right, subRoot);
     }
 
     public static boolean verifySubTree(TreeNode root, TreeNode subRoot) {
         if (root == null && subRoot == null) return true;
         if (root != null && subRoot != null && root.val == subRoot.val) {
-                return verifySubTree(root.left, subRoot.left) && verifySubTree(root.right, subRoot.right);
+            return verifySubTree(root.left, subRoot.left) && verifySubTree(root.right, subRoot.right);
         }
 
         return false;

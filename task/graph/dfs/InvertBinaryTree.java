c83f97e7ce5d28b3c4b47713ed0f10cf071e3a8e
Oleh <myhash.oleg@gmail.com>
2021-12-02 23:04:06 +0200

    add invert binary tree task

--- task/graph/dfs/InvertBinaryTree.java
+++ task/graph/dfs/InvertBinaryTree.java
@@ -47,8 +47,7 @@ public class InvertBinaryTree {
                 new TreeNode(2, new TreeNode(1), new TreeNode(3)),
                 new TreeNode(7, new TreeNode(6), new TreeNode(9)));
 
-//        var inverted = invertTree(tree);
-        var invertedIter = invertTreeIterative(tree);
-        System.out.println();
+        invertTree(tree);
+        invertTreeIterative(tree);
     }
 }

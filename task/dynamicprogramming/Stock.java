3990e127c77f98742730596f3563d081314710a3
oleh <myhash.oleg@gmail.com>
2022-02-22 12:10:16 +0200

    stock written from scratch

--- task/dynamicprogramming/Stock.java
+++ task/dynamicprogramming/Stock.java
@@ -2,14 +2,12 @@ package dynamicprogramming;
 
 public class Stock {
     public static void main(String[] args) {
-//        System.out.println(maxProfit(new int[] {7,1,5,3,6,4}));
-//        System.out.println(maxProfit(new int[] {7,6,4,3,1}));
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
         System.out.println(maxProfit(new int[]{8, 5, 2, 4, 54, 1, 3, 12, 32, 4, 5, 6, 7, 7, 43, 4, 2}));
     }
 
     public static int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) return 0;
-
         int left = 0;
         int right = 1;
         int maxProfit = 0;
@@ -18,10 +16,8 @@ public class Stock {
             if (prices[right] > prices[left]) {
                 maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
             } else left = right;
-
             right++;
         }
-
         return maxProfit;
     }
 }

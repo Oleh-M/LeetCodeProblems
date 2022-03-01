7b03a518b01ac65eca9b1686b16b292c122581c7
oleh <myhash.oleg@gmail.com>
2022-03-01 18:29:33 +0200

    Rewrite MaxSubArray task

--- task/dynamicprogramming/MaxSubArray.java
+++ task/dynamicprogramming/MaxSubArray.java
@@ -4,20 +4,20 @@ public class MaxSubArray {
 
     public static void main(String[] args) {
         System.out.println(maxSubArray(new int[]{-1, -2}));
+        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println(maxSubArray(new int[]{1}));
+        System.out.println(maxSubArray(new int[]{5, 4, -1, 7, 8}));
     }
 
     public static int maxSubArray(int[] nums) {
         if (nums.length == 1) return nums[0];
+        int currentSum = 0, longestSum = nums[0];
 
-        int longestSum = nums[0];
-        int currentSum = 0;
-
-        for (int n : nums) {
+        for (int num : nums) {
             if (currentSum < 0) {
                 currentSum = 0;
             }
-
-            currentSum += n;
+            currentSum += num;
             longestSum = Math.max(longestSum, currentSum);
         }
 

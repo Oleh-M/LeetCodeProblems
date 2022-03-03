569a0cccfd641b115770a708c98c68f635349174
oleh <myhash.oleg@gmail.com>
2022-03-03 16:04:25 +0200

    Rewrite SearchInRotatedSortedArray task

--- task/binarysearch/SearchInRotatedSortedArray.java
+++ task/binarysearch/SearchInRotatedSortedArray.java
@@ -15,22 +15,33 @@ public class SearchInRotatedSortedArray {
     }
 
     public static int search(int[] nums, int target) {
-        int mid, left = 0, right = nums.length - 1;
+        if (nums == null || nums.length == 0) return -1;
+
+        int left = 0, right = nums.length - 1;
+        int mid;
+
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else right = mid;
+        }
+
+        int start = left;
+        left = 0;
+        right = nums.length - 1;
+
+        if (nums[start] <= target && nums[right] >= target) {
+            left = start;
+        } else right = start;
 
         while (left <= right) {
             mid = left + (right - left) / 2;
-            if (nums[mid] == target) return mid;
-
-            else if (nums[mid] >= nums[left]) { // right pivot
-                if (target < nums[left] || target > nums[mid]) {
-                    left = mid + 1;
-                } else right = mid - 1;
-
-            } else if (nums[mid] < nums[right]) { // left pivot
-                if (nums[right] < target || target < nums[mid]) {
-                    right = mid - 1;
-                } else left = mid + 1;
-            }
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else return mid;
         }
 
         return -1;

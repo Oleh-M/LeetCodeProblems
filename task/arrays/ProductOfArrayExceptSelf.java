8cdaa52f13d6d504c1d1f638fda56e28d4d3e93f
oleh <myhash.oleg@gmail.com>
2022-03-07 16:23:53 +0200

    Rewrite ProductOfArrayExceptSelf task

--- task/arrays/ProductOfArrayExceptSelf.java
+++ task/arrays/ProductOfArrayExceptSelf.java
@@ -11,23 +11,23 @@ public class ProductOfArrayExceptSelf {
     }
 
     public static int[] productExceptSelf(int[] nums) {
-        int[] leftProduct = new int[nums.length];
-        int[] rightProduct = new int[nums.length];
+        int[] leftProd = new int[nums.length];
+        int[] rightProd = new int[nums.length];
         int[] result = new int[nums.length];
 
-        leftProduct[0] = 1;
-        rightProduct[nums.length - 1] = 1;
+        leftProd[0] = 1;
+        rightProd[nums.length - 1] = 1;
 
         for (int i = 1; i < nums.length; i++) {
-            leftProduct[i] = leftProduct[i - 1] * nums[i - 1];
+            leftProd[i] = leftProd[i - 1] * nums[i - 1];
         }
 
         for (int i = nums.length - 2; i >= 0; i--) {
-            rightProduct[i] = rightProduct[i + 1] * nums[i + 1];
+            rightProd[i] = rightProd[i + 1] * nums[i + 1];
         }
 
         for (int i = 0; i < nums.length; i++) {
-            result[i] = leftProduct[i] * rightProduct[i];
+            result[i] = leftProd[i] * rightProd[i];
         }
 
         return result;

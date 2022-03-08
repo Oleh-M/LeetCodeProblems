b93713c200f2bbe070d98ecdd0197ee03ee52de3
oleh <myhash.oleg@gmail.com>
2022-03-08 14:04:37 +0200

    Rewrite MaxProductSubArray task

--- task/dynamicprogramming/MaxProductSubArray.java
+++ task/dynamicprogramming/MaxProductSubArray.java
@@ -3,25 +3,29 @@ package dynamicprogramming;
 public class MaxProductSubArray {
 
     public static int maxProduct(int[] nums) {
-        int max = nums[0];
-        int currMax = 1, currMin = 1;
+        if (nums.length == 1) return nums[0];
+        int max = nums[0],
+                currentMax = 1,
+                currentMin = 1;
 
-        for (int n : nums) {
-            if (n == 0) {
-                currMax = 1;
-                currMin = 1;
+        for (int i : nums) {
+            if (i == 0) {
+                currentMax = 1;
+                currentMin = 1;
             }
-            int tmp = currMax * n;
-            currMax = Math.max(tmp, Math.max(currMin * n, n));
-            currMin = Math.min(tmp, Math.min(currMin * n, n));
 
-            max = Math.max(max, Math.max(currMax, currMin));
+            int tmp = currentMax * i;
+            currentMax = Math.max(tmp, Math.max(currentMin * i, i));
+            currentMin = Math.min(tmp, Math.min(currentMin * i, i));
+
+            max = Math.max(max, Math.max(currentMax, currentMin));
         }
 
         return max;
     }
 
     public static void main(String[] args) {
+        System.out.println(maxProduct(new int[]{2, -5, -2, -4, 3}));
         System.out.println(maxProduct(new int[]{2, 3, -2, 4}));
         System.out.println(maxProduct(new int[]{-2, 0, -1}));
         System.out.println(maxProduct(new int[]{3, -1, 4}));

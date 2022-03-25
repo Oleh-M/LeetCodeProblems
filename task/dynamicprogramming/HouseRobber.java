e8e32900c5009c83ef7f535c273893d07f8468d0
oleh <myhash.oleg@gmail.com>
2022-03-25 17:10:09 +0200

    Rewrite HouseRobber task

--- task/dynamicprogramming/HouseRobber.java
+++ task/dynamicprogramming/HouseRobber.java
@@ -3,16 +3,16 @@ package dynamicprogramming;
 public class HouseRobber {
 
     public static int rob(int[] nums) {
-        int rob = 0,
-                notRob = 0;
+        if (nums == null || nums.length == 0) return 0;
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
 
-        for (int num : nums) {
-            int currRob = notRob + num;
-            notRob = Math.max(rob, notRob);
-            rob = currRob;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
         }
 
-        return Math.max(rob, notRob);
+        return dp[nums.length];
     }
 
     public static void main(String[] args) {

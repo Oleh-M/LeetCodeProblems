558721aadc86b20391d5b262820a1893f206fdfc
oleh <myhash.oleg@gmail.com>
2022-03-29 19:01:57 +0300

    Rewrite CourseSchedule task

--- task/graph/CourseSchedule.java
+++ task/graph/CourseSchedule.java
@@ -9,43 +9,38 @@ public class CourseSchedule {
 
         // create the array lists to represent the courses
         var courses = new ArrayList<List<Integer>>(numCourses);
+
         for (int i = 0; i < numCourses; i++) {
-            courses.add(new ArrayList<>());
+            courses.add(new ArrayList<>(2));
+        }
+        // create the dependency for courses
+        for (int[] prerequisite : prerequisites) {
+            courses.get(prerequisite[1]).add(prerequisite[0]);
         }
 
-        // create the dependency graph
-        for (int i = 0; i < prerequisites.length; i++) {
-            courses.get(prerequisites[i][1]).add(prerequisites[i][0]);
-        }
+        var visited = new int[numCourses];
 
-        int[] visited = new int[numCourses];
-
-        // dfs visit each course
         for (int i = 0; i < numCourses; i++) {
-            if (!dfs(i, courses, visited)) return false;
+            if (!dfs(courses, visited, i)) return false;
         }
 
         return true;
     }
 
-    private static boolean dfs(int course,
-                               List<List<Integer>> courses,
-                               int[] visited) {
-        visited[course] = 1; // mark that course being visited
-        var eligibleCourses = courses.get(course); // get its children
+    private static boolean dfs(List<List<Integer>> courses, int[] visited, int course) {
+        visited[course] = 1;
 
-        // dfs courses
-        for (int i = 0; i < eligibleCourses.size(); i++) {
-            int eligibleCourse = eligibleCourses.get(i);
+        var eligibleCourses = courses.get(course);
 
-            // course has been visited while visiting its children (i.e. cycle)
+        for (Integer eligibleCourse : eligibleCourses) {
             if (visited[eligibleCourse] == 1) return false;
-            if (visited[eligibleCourse] == 0) {// not visited
-                if (!dfs(eligibleCourse, courses, visited)) return false;
+            if (visited[eligibleCourse] == 0) {
+                if (!dfs(courses, visited, eligibleCourse)) return false;
             }
         }
 
-        visited[course] = 2; // mark it done visiting (any number except 1 and 0)
+        visited[course] = 2;
+
         return true;
     }
 

33b9153672ed06c554034cde963c53f82f8430da
oleh <myhash.oleg@gmail.com>
2022-06-23 15:36:57 +0300

    Add rewrite ConnectedComponents task

--- task/graph/ConnectedComponents.java
+++ task/graph/ConnectedComponents.java
@@ -1,25 +1,26 @@
 package graph;
 
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class ConnectedComponents {
     public static int findCircleNumBFS(int[][] isConnected) {
-        Queue<Integer> queue = new LinkedList<>();
+        var nodesToVisit = new LinkedList<Integer>();
         var visited = new boolean[isConnected.length];
         var counter = 0;
 
         for (int i = 0; i < isConnected.length; i++) {
             if (!visited[i]) {
-                queue.add(i);
+                nodesToVisit.add(i);
                 counter++;
             }
-            while (!queue.isEmpty()) {
-                var current = queue.poll();
+
+            while (!nodesToVisit.isEmpty()) {
+                var current = nodesToVisit.poll();
                 visited[current] = true;
                 for (int j = 0; j < isConnected[0].length; j++) {
-                    if (j != current && isConnected[current][j] == 1 && !visited[j])
-                        queue.add(j);
+                    if (!visited[j] && isConnected[current][j] == 1 && current != j) {
+                        nodesToVisit.add(j);
+                    }
                 }
             }
         }
@@ -29,30 +30,29 @@ public class ConnectedComponents {
 
     public static int findCircleNumDFS(int[][] isConnected) {
         var visited = new boolean[isConnected.length];
-        int counter = 0;
+        var counter = 0;
 
         for (int i = 0; i < isConnected.length; i++) {
             if (!visited[i]) {
                 counter++;
-                findCycleDfs(isConnected, visited, i);
+                findCircleNumRec(isConnected, visited, i);
             }
         }
 
         return counter;
     }
 
-    private static void findCycleDfs(int[][] graph, boolean[] visited, int current) {
+    private static void findCircleNumRec(int[][] graph, boolean[] visited, int current) {
+        visited[current] = true;
         for (int i = 0; i < graph[0].length; i++) {
-            if (!visited[i] && graph[current][i] != 0) {
-                visited[i] = true;
-                findCycleDfs(graph, visited, i);
+            if (!visited[i] && graph[current][i] == 1 && current != i) {
+                findCircleNumRec(graph, visited, i);
             }
         }
     }
 
     public int findCircleNumUnion(int[][] isConnected) {
         var unionFind = new UnionFind(isConnected.length);
-        var counter = isConnected.length;
 
         for (int i = 0; i < isConnected.length; i++) {
             for (int j = i; j < isConnected[i].length; j++) {
@@ -66,45 +66,52 @@ public class ConnectedComponents {
     }
 
     public static void main(String[] args) {
-        findCircleNumBFS(new int[][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}});
-        findCircleNumBFS(new int[][]{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}});
+        System.out.println(findCircleNumBFS(new int[][]{{1, 0, 0, 1}, {0, 1, 1, 0}, {0, 1, 1, 1}, {1, 0, 1, 1}}));
+        System.out.println(findCircleNumBFS(new int[][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}));
+        System.out.println(findCircleNumBFS(new int[][]{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}));
     }
 }
 
 class UnionFind {
-    private int[] parent;
-    private int[] rank;
-    private int numberOfDisconnectedEdges;
+    private int size;
+    private int[] ids;
+    private int[] sz;
+    private int numConnected;
 
     public UnionFind(int size) {
-        this.numberOfDisconnectedEdges = size;
-        this.parent = new int[size];
+        this.size = size;
+        this.ids = new int[size];
+        this.sz = new int[size];
+
         for (int i = 0; i < size; i++) {
-            this.parent[i] = i;
-        }
-        this.rank = new int[size];
-    }
-
-    public int find(int x) {
-        if (this.parent[x] != x) this.parent[x] = find(this.parent[x]);
-        return this.parent[x];
-    }
-
-    public boolean union(int x, int y) {
-        int xr = this.find(x), yr = this.find(y);
-        if (xr == yr) return false;
-        else if (this.rank[xr] < this.rank[yr]) this.parent[xr] = yr;
-        else if (this.rank[xr] > this.rank[yr]) this.parent[yr] = xr;
-        else {
-            this.parent[yr] = xr;
-            this.rank[xr]++;
+            ids[i] = i;
+            sz[i] = 1;
         }
 
-        this.numberOfDisconnectedEdges--;
-        return true;
+        this.numConnected = size;
+    }
+
+    public int find(int p) {
+        if (p == ids[p]) return p;
+        else return ids[p] = find(ids[p]);
+    }
+
+    public void union(int u, int v) {
+        u = find(u);
+        v = find(v);
+        if (u == v) return;
+
+        if (sz[u] > sz[v]) {
+            ids[v] = u;
+            sz[u] += sz[v];
+        } else {
+            ids[u] = v;
+            sz[v] += sz[u];
+        }
+        this.numConnected--;
     }
 
     public int getNumberOfDisconnectedEdges() {
-        return this.numberOfDisconnectedEdges;
+        return numConnected;
     }
 }

79fd217bc5f30b35c3fdde2cc7c83123b05c9df7
oleh <myhash.oleg@gmail.com>
2022-08-12 14:33:43 +0300

    Rewrite MergeIntervals

--- task/interval/MergeIntervals.java
+++ task/interval/MergeIntervals.java
@@ -10,19 +10,19 @@ public class MergeIntervals {
             return intervals;
         }
 
-        Arrays.sort(intervals, Comparator.comparingInt(arr -> arr[0]));
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
 
         var result = new ArrayList<int[]>();
         var currInterval = intervals[0];
         result.add(currInterval);
 
         for (int[] interval : intervals) {
-            var curEnd = currInterval[1];
+            var currEnd = currInterval[1];
             var nextStart = interval[0];
             var nextEnd = interval[1];
 
-            if (curEnd >= nextStart) {
-                currInterval[1] = Math.max(curEnd, nextEnd);
+            if (currEnd >= nextStart) {
+                currInterval[1] = Math.max(currInterval[1], nextEnd);
             } else {
                 result.add(interval);
                 currInterval = interval;
@@ -35,5 +35,6 @@ public class MergeIntervals {
     public static void main(String[] args) {
         merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}});
         merge(new int[][]{{1, 4}, {5, 6}});
+        merge(new int[][]{{1, 4}, {4, 6}});
     }
 }

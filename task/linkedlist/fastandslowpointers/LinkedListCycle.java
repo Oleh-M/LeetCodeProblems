a1c169286baa23ac81872c5747867533705ab6cc
oleh <myhash.oleg@gmail.com>
2022-08-15 12:15:51 +0300

    Rewrite LinkedListCycle task

--- task/linkedlist/fastandslowpointers/LinkedListCycle.java
+++ task/linkedlist/fastandslowpointers/LinkedListCycle.java
@@ -1,4 +1,4 @@
-package fastandslowpointers;
+package linkedlist.fastandslowpointers;
 
 import fastandslowpointers.addTwoNumbers.ListNode;
 
@@ -6,19 +6,20 @@ public class LinkedListCycle {
 
     public static void main(String[] args) {
         System.out.println(hasCycle(
-                new ListNode(3, new ListNode(2, new ListNode(0, new ListNode(-4, new ListNode(1)))))
+                new ListNode(3, new ListNode(2, new ListNode(0, new ListNode(-4, new ListNode(2)))))
         ));
     }
 
     public static boolean hasCycle(ListNode head) {
-        ListNode slow = head, fast = head;
+        if (head == null || head.next == null) return false;
+        if (head == head.next) return true;
+
+        ListNode slow = head, fast = head.next;
 
         while (fast != null && fast.next != null) {
+            if (fast == slow) return true;
             fast = fast.next.next;
             slow = slow.next;
-            if (fast == slow) {
-                return true;
-            }
         }
 
         return false;
